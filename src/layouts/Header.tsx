81af0fd514566932095154b8936eff58aac6bc93
Alex Sousa <alexsousa@byteshouse.com>
2025-03-26 16:37:46 +0000

    add styles to `header`

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,50 @@
+import logoIgnite from "../assets/logo-ignite.svg";
+import { Scroll, Timer } from "phosphor-react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
   return (
-    <div>
-      <h1>Header component</h1>
-    </div>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="logo" />
+      <nav>
+        <NavLink to="/" title="Temporizador">
+          <Timer size={28} />
+        </NavLink>
+        <NavLink to="history" title="Histórico">
+          <Scroll size={28} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   );
 }
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: ${({ theme }) => theme.colors["gray-100"]};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${({ theme }) => theme.colors["green-500"]};
+      }
+      &.active {
+        color: ${({ theme }) => theme.colors["green-500"]};
+      }
+    }
+  }
+`;
--- src/layouts/LayoutDefault.tsx
+++ src/layouts/LayoutDefault.tsx
@@ -1,11 +1,24 @@
 import { Header } from "./Header.tsx";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 export function LayoutDefault() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   );
 }
+
+const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background-color: ${({ theme }) => theme.colors["gray-800"]};
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+`;
